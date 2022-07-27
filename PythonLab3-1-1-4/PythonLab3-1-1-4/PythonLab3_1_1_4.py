bdbc9fe6c9aa9810c3a255a6e53c4552d4b0c758
Harvey1968 <108738675+Harvey1968@users.noreply.github.com>
2022-07-27 14:23:23 +0100

    Add files via upload

---
+++ PythonLab3-1-1-4/PythonLab3-1-1-4/PythonLab3_1_1_4.py
@@ -0,0 +1,7 @@
+#3.1.1.4 LAB: Questions and answers
+
+#User inputs an integer
+n = int(input("Enter an integer: "))
+#If integer is less than 100 return False,
+#If integer is greater than or equal to 100 return True.
+print("The integer entered is equal or greater than 100 is...", n >= 100, "\n") # Equal or greater than
